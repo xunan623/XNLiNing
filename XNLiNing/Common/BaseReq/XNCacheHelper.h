47d5f8280a88d71b1b7d181ddd1865d71ebaabf8
xunan <xunan@pingchang666.com>
2018-03-06 09:51:31 +0800

    YYCache网络缓存框架

---
+++ XNLiNing/Common/BaseReq/XNCacheHelper.h
@@ -0,0 +1,13 @@
+//
+//  XNCacheHelper.h
+//  XNLiNing
+//
+//  Created by Dandre on 2018/3/5.
+//  Copyright © 2018年 xunan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface XNCacheHelper : NSObject
+
+@end
---
+++ XNLiNing/Common/BaseReq/XNCacheHelper.m
@@ -0,0 +1,13 @@
+//
+//  XNCacheHelper.m
+//  XNLiNing
+//
+//  Created by Dandre on 2018/3/5.
+//  Copyright © 2018年 xunan. All rights reserved.
+//
+
+#import "XNCacheHelper.h"
+
+@implementation XNCacheHelper
+
+@end
